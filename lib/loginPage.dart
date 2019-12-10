0a13ed831301c4bc2f6717268a5c89c1897c3f6e
torrex01 <iagulloc@alumnos.ceduc.cl>
2019-12-10 15:52:46 -0300

    carga inicial

---
+++ lib/loginPage.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'MenuPricipal.dart';
+
+class LoginPage extends StatelessWidget {
+  Widget renderEmpresamput(){
+    return Padding(
+               padding: const EdgeInsets.only(top: 40),
+               child: TextFormField(
+                decoration: InputDecoration(hintText:'Empresa'),
+            ),
+             );
+  }
+  Widget renderEmailImput(){
+    return Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: TextFormField(
+                decoration: InputDecoration(hintText:'Usuario o Correo electrobico'),
+              ),
+            );
+  }
+  Widget renderPasswordImput(){
+    return Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: TextFormField(
+                decoration: InputDecoration(hintText:'Contraseña'),
+                obscureText: true,
+              ),
+            );
+  }
+
+  Widget renderLoginbutton(BuildContext context){
+    return Container(padding: const EdgeInsets.only(top:32),
+    child: RaisedButton(
+      textColor: Colors.white,
+      child: Text('Entrar'),
+      onPressed: () {
+       Navigator.push(context,
+                MaterialPageRoute(builder: (context) => MenuPrincipal())); 
+                
+      },
+    ));
+  }
+
+  Widget renderCreateAccountLink(){
+    return Container(
+      padding: EdgeInsets.only(top: 20),
+      child: Text(
+         'O crea tu cuenta aqui',
+          textAlign: TextAlign.right,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ));
+  }
+
+  Widget renderDivisor(){
+    return Container(
+      padding: const EdgeInsets.only(top: 32),
+      child: Row(
+        children:[
+        Expanded(child: Divider(height: 1)),
+        Divider(height: 1),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Text('0'),
+        ),
+        Expanded(child: Divider(height: 1)),
+      ])
+    );
+  }
+
+   
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(color: Colors.white),
+        child: ListView(children: [
+            Image.asset('assets/images/download.jpg',
+            width: 630.0,
+            ),
+            renderEmpresamput(),
+            renderEmailImput(),
+            renderPasswordImput(),
+            renderLoginbutton(context),
+            renderCreateAccountLink(),
+            renderDivisor(),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/ventas.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Ventas extends StatefulWidget {
+
+  
+  @override
+  _Ventas createState() => new _Ventas();
+}
+class _Ventas extends State<Ventas> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+            title: Text("Menu Principal"),
+            backgroundColor: Colors.red, 
+          ),
+          );
+      }
+    }
